ebd14614ef5e52f41fb43075cb801e05baf16ea8
juancastillam98 <castillamariscal.juan@gmail.com>
2022-10-13 18:33:52 +0200

    Ejercicios 1 y 2

--- DWES_01_Formulario/practica01/funciones/ejercicio1.php
+++ DWES_01_Formulario/practica01/funciones/ejercicio1.php
@@ -1,9 +1,10 @@
 <?php
-function sacarPrimos($cantidad,$incio){
-    echo "hola";
+function sacarPrimos($cantidad, $incio)
+{
     $i = $incio;
     $totalPrimos = 0;
-    $res="";
+    $res = "";
+
     do {
         $primo = true;
         $i++;
@@ -14,11 +15,104 @@ function sacarPrimos($cantidad,$incio){
         }
 
         if ($primo) {
+            $res = $res . "$i <br/>";
             //echo "$i <br/>";
-            $res+= "$i <br/>";
             $totalPrimos++;
         }
     } while ($totalPrimos != $cantidad);
-    return $totalPrimos;
+    return $res;
 }
-?>
+
+function comprobarDni($dni)
+{
+    //5359989G
+    $dniValido = true;
+    $digitos = "0123456789";
+    $letras = "abcdefghijklmnnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVW";
+    $numeros = "";
+    $hay9Caracteres = true;
+    $hay8CaracteresIniciales = true;
+    $primeros8Numeros = true;
+    $hayUnaLetra = true;
+
+    //1. Compruebo que hay 9 caracteres
+    if (strlen($dni) != 9) {
+        $hay9Caracteres = false;
+    }
+    //echo $hay9Caracteres ? "hay 9 caracteres"  : "no hay 9 caracteres";
+
+    //2. Compruebo que haya 8 caracteres iniciales
+    if (strlen($dni) - 1 != 8) {
+        $hay8CaracteresIniciales = false;
+    }
+    //echo $hay8CaracteresIniciales ? "hay 8 caracteres"  : "hay letras";
+
+    //3. Compruebo que los 8 caracteres iniciales son dígitos
+    //3.2 Aprovecho este bucle para sacar los 8 dígitos.
+    for ($i = 0; $i < strlen($dni) - 1; $i++) {
+        if (!str_contains($digitos, $dni[$i])) {
+            $primeros8Numeros = false;
+        }
+        $numeros .= $dni[$i];
+    }
+    //echo $primeros8Numeros ? "solo digitos"  : "hay letras";
+
+    //4. Compruebo que el 9º caracter es una letra
+    if (!str_contains($letras, $dni[8])) {
+        $hayUnaLetra = false;
+    }
+    //echo $hayUnaLetra ? "hay una letra"  : "no es una letra";
+
+    //5. Si se cumplen las 4 condiciones anteriores, paso ompruebo que la letra es válida.
+    if ($hay9Caracteres && $hay8CaracteresIniciales && $primeros8Numeros && $hayUnaLetra) {
+        $numeros = (int)$numeros;
+        $resto = $numeros % 23;
+        $letra = match ($resto) {
+            0 => "T",
+            1 => "R",
+            2 => "w",
+            3 => "A",
+            4 => "G",
+            5 => "N",
+            6 => "Y",
+            7 => "F",
+            8 => "P",
+            9 => "D",
+            10 => "X",
+            11 => "B",
+            12 => "N",
+            13 => "J",
+            14 => "Z",
+            15 => "S",
+            16 => "Q",
+            17 => "V",
+            18 => "H",
+            19 => "L",
+            20 => "C",
+            21 => "K",
+            22 => "E",
+        };
+        //echo "<br/> Tu letra es $letra <br/>";
+        if (strtoupper($dni[8]) !== $letra) {
+            $dniValido = false;
+        }
+    } else {
+        $dniValido = false;
+    }
+    return  $dniValido;
+}
+
+function TablasMultiplicar1Al10()
+{
+    echo "<table>";
+    for ($i = 1; $i <= 10; $i++) {
+        echo "<tr>";
+        for ($j = 1; $j <= 10; $j++) {
+            echo"<td>";
+            echo "$i x $j = ".($i*$j);
+            echo "</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+}
--- DWES_01_Formulario/practica01/index.php
+++ DWES_01_Formulario/practica01/index.php
@@ -11,9 +11,70 @@
 
 <body>
     <h2>Ejercicio 1</h2>
-    <?php 
-    require "funciones/ejercicio1.php";
-    echo sacarPrimos(3, 4);
+    <div id="ejercicio1">
+        <form action="" method="POST">
+            <label for="num1">Introduce un número de inicio:
+                <input type="text" name="num1"><br />
+            </label>
+            <label for="num2">Introduce la cantidad de primos a devolver:
+                <input type="text" name="num2"><br />
+            </label>
+            <input type="hidden" name="f" value="ej1"><br />
+            <input type="submit" name="btnEnviar" value="Enviar">
+        </form>
+
+        <?php
+        //funciones 
+        require "funciones/ejercicio1.php";
+
+        //Ejercicio 1;
+        /* $primos = sacarPrimos(3, 4);
+        echo $primos; */
+        $num1 = 0;
+        $num2 = 0;
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["btnEnviar"])) {
+            if ($_POST["f"] == "ej1") {
+                $num1 = $_POST["num1"];
+                $num1 = $_POST["num2"];
+            }
+        }
+        $primos = sacarPrimos($num1, $num2);
+        echo $primos;
+
+        ?>
+    </div>
+
+
+    <h2>Ejercicio 2</h2>
+    <div id="ejercicio2">
+        <form action="" method="POST">
+            <label for="dni">Introduce un DNI:
+                <input type="text" name="dni"><br />
+            </label>
+            <input type="hidden" name="f" value="ej2"><br />
+            <input type="submit" name="btnEnviar" value="Enviar">
+        </form>
+
+        <?php
+        //Ejercicio2
+        //echo comprobarDni("53599848G");
+        $dniIntroducido = "";
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["btnEnviar"])) {
+            if ($_POST["f"] == "ej2") {
+                $dniIntroducido = $_POST["dni"];
+            }
+        }
+
+        if (comprobarDni($dniIntroducido)) {
+            echo "El dni es válido <br/>";
+        } else {
+            echo "El dni introducido no es correcto <br/>";
+        }
+        ?>
+    </div>
+    <h2>Ejercicio 3</h2>
+    <?php
+
     ?>
 </body>
 
