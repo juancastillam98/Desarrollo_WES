7a9b9207c1c0cf959b0b9e6c63e5516a3f647157
juancastillam98 <castillamariscal.juan@gmail.com>
2022-10-20 17:45:08 +0200

    Segunda parte validación

--- DWES_03_ValidacionFormularios/usuarios/index.php
+++ DWES_03_ValidacionFormularios/usuarios/index.php
@@ -19,9 +19,17 @@
         $temp_nombre = depurar($_POST["nombre"]);
         $temp_apellidos = depurar($_POST["apellidos"]);
         $temp_dni = depurar($_POST["dni"]);
+        $temp_email = depurar($_POST["email"]);
+        $temp_fecha = depurar($_POST["fecha"]);
 
         $patternNombre = "/^[a-zA-Z áéíóúÑñ]+$/";
         $patternDni = "/^[0-9]{8}[a-zA-Z ]{1}+$/";
+        //Fecha:
+        /**EL día solo puede empezar por 0,1,2 o 3.
+         * El mes por 0 o 1
+         * Y el año por 20 o 19
+         */
+        $patternFecha = "/^[0-3][0-9][\/][0-1][0-2][\/](19|20)[0-9]{2}+$/";
 
         //Comprobación del nombre
         if (strlen($temp_nombre) < 2 || strlen($temp_nombre) > 40) {
@@ -44,7 +52,7 @@
                 $apellidos = $temp_apellidos;
             }
         }
-
+        //Validación del DNI
         if (empty($temp_dni)) {
             $error_dni = "* El dni es obligatorio";
         } else {
@@ -53,6 +61,22 @@
             } else {
                 $dni = $temp_dni;
             }
+        }
+
+        //Comprobación del email
+        //Validación de Email
+        $emailCorrecto = filter_var($temp_email, FILTER_VALIDATE_EMAIL);
+        if (!$emailCorrecto) {
+            $error_email = "* El email introducido es incorrecto";
+        } else {
+            $email = $temp_email;
+        }
+
+        //Validación fecha
+        if (!preg_match($patternFecha, $temp_fecha)) {
+            $error_fecha = "* La fecha introducida no tiene el formato adecuado (dd/MM/YYYY)";
+        } else {
+            $fecha = $temp_fecha;
         }
     }
 
@@ -74,9 +98,21 @@
         <span class="error">
             <?php if (isset($error_apellidos)) echo $error_apellidos ?>
         </span>
+
         <p>Dni: <input type="text" name="dni" /></p>
         <span class="error">
             <?php if (isset($error_dni)) echo $error_dni ?>
+        </span>
+        <!--Email-->
+        <p>Email: <input type="text" name="email" /></p>
+        <span class="error">
+            <?php if (isset($error_email)) echo $error_email ?>
+        </span>
+
+        <!--Fecha-->
+        <p>Fecha: <input type="text" name="fecha" /></p>
+        <span class="error">
+            <?php if (isset($error_fecha)) echo $error_fecha ?>
         </span>
 
 
